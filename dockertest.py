ab2ef56e188a1f271b31b6fa6f2e7c49cd4be501
jimdibb <jimdibb@gmail.com>
2018-05-01 23:11:00 -0400

    added logging

--- dockertest.py
+++ dockertest.py
@@ -1,5 +1,10 @@
 
 import sys
+import logging
 
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+logging.info("test started")
+
+logging.info(str(sys.argv))
 print (len(sys.argv))
 print (str(sys.argv))
